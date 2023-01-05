4417716c0cf9093d47ddb26a7856cbc6454e24f1
agedz1911 <71722761+agedz1911@users.noreply.github.com>
2023-01-05 10:34:31 +0700

    Initial commit

---
+++ lib/app/modules/add_pegawai/controllers/add_pegawai_controller.dart
@@ -0,0 +1,52 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AddPegawaiController extends GetxController {
+  TextEditingController nameC = TextEditingController();
+  TextEditingController nipC = TextEditingController();
+  TextEditingController emailC = TextEditingController();
+
+  FirebaseAuth auth = FirebaseAuth.instance;
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  void addPegawai() async {
+    if (nameC.text.isNotEmpty && nipC.text.isNotEmpty && emailC.text.isNotEmpty) {
+      try {
+        final credential = await auth.createUserWithEmailAndPassword(
+          email: emailC.text,
+          password: "password",
+        );
+
+        if(credential.user != null ){
+          String uid = credential.user!.uid;
+          
+          await firestore.collection("pegawai").doc(uid).set({
+            "nip" : nipC.text,
+            "name" : nameC.text,
+            "email" : emailC.text,
+            "uid" : uid,
+            "createdAt" : DateTime.now().toIso8601String(),
+          });
+        }
+
+        print(credential);
+
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'weak-password') {
+          Get.snackbar("Error", "Mohon gunakan password yang lebih kuat");
+          //print('The password provided is too weak.');
+        } else if (e.code == 'email-already-in-use') {
+          Get.snackbar("Error", "Email sudah terdaftar, kamu tidak dapat menambahkan dengan email ini");
+          //print('The account already exists for that email.');
+        }
+      } catch (e) {
+        //print(e);
+        Get.snackbar("Error", "Tidak dapat menambahkan pegawai");
+      }
+    } else {
+      Get.snackbar("Error", "NIP, Nama, dan Email harus diisi");
+    }
+  }
+}
---
+++ lib/app/modules/home/views/home_view.dart
@@ -0,0 +1,31 @@
+import 'package:absensi/app/routes/app_pages.dart';
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/home_controller.dart';
+
+class HomeView extends GetView<HomeController> {
+  const HomeView({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomeView'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () => Get.toNamed(Routes.ADD_PEGAWAI), 
+            icon: Icon(Icons.person_add),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text(
+          'HomeView is working',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
